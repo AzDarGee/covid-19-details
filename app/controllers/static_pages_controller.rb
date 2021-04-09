14efd024e38206cc405faa56203dc51aace3119c
ASHISH DARJI <ashdarji1@gmail.com>
2021-04-09 17:35:25 -0400

    getting and scrubbing data, formatting, styling

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,50 @@
 class StaticPagesController < ApplicationController
-  
+  require 'net/http'
+  require 'json'
+
   def home
     # @countries = Country.all
+    # 1. Get list of 10 countries with the MOST cases of COVID-19 - DONE
+    # 2. Access details for each of those 10 countries - 
+
+
+    # Get summary for all countries
+    summaryURL = URI('https://api.covid19api.com/summary')
+
+    # Parse the request
+    summary = JSON.parse(Net::HTTP.get(summaryURL))
+
+    # Date of Request
+    @date = summary["Date"]
+
+    # Global Data
+    @globalData = summary["Global"]
+
+    # Summary of All Countries - unsorted
+    countries = summary["Countries"]
+
+    # Sort by Most COVID-19 cases
+    @sortedCountries = countries.sort_by { |country| country['TotalConfirmed'].to_i }
+    @sortedCountries.reverse!
+
+    # Get the first 10 countries
+    @mostCases = @sortedCountries.slice(0,10)
+    
+    # Recent Country Details
+    countryDetails = []
+    detailsURL = "https://api.covid19api.com/total/country/"
+    @recentCountryData = []
+
+    for country in @mostCases 
+      countryDetails.append(JSON.parse(Net::HTTP.get( URI(detailsURL + country['CountryCode']))))
+    end
+    
+    for country in countryDetails
+      @recentCountryData.append(country.last)
+    end
+    
+    
+
   end
 
 end
